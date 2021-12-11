b9bad7dbcc66b0a03fdea0fb328ac353110a4611
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2021-12-11 15:16:39 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# password generator
+# by yara elsakka
+#
+# 06.12.2021
+
+from string import *
+
+print("password generator")
+
+allLetters = ascii_letters
+num = digits
+speCh = punctuation
+passwordsalad = [allLetters, num, speCh]
+
+print(passwordsalad)
+
+
